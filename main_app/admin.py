2b290f51139746ad92df2b4e0035012770dd2115
nataliel2514 <natalielopez2514@gmail.com>
2021-10-07 12:04:31 -0500

    add song to admin portal

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import gyms
+from .models import gyms, location
 # Register your models here.
 admin.site.register(gyms)
+admin.site.register(location)
