c60db1902da3db553ad672f641cd60a859d4451f
nataliel2514 <natalielopez2514@gmail.com>
2021-10-06 23:31:21 -0500

    model display

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.http import HttpResponse
+from .models import gyms
+
 
 class Home(TemplateView):
     template_name = "home.html"
-    
+
+
 class About(TemplateView):
     template_name = "about.html"
-    
+
 
 # other
 class GymsList(TemplateView):
     template_name = "gyms_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Here we are using the model to query the database for us.
+        context["gyms"] = gyms.objects.all()
+        return context
